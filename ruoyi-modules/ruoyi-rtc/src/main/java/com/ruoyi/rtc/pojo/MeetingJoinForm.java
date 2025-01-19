4f4d91adaa4d433952c6e93c8a4c4a44074fc11d
dmzhang <z50610233@gmail.com>
2025-01-19 16:28:00 +0800

    refactor(rtc): 重构 MeetingJoinForm 并删除 OfferMessage 类
    - 使用 Lombok 注解简化 MeetingJoinForm 类的代码结构- 添加链式访问器以提高代码可读性和易用性- 删除未使用的 OfferMessage 类，减少冗余代码

--- ruoyi-modules/ruoyi-rtc/src/main/java/com/ruoyi/rtc/pojo/MeetingJoinForm.java
+++ ruoyi-modules/ruoyi-rtc/src/main/java/com/ruoyi/rtc/pojo/MeetingJoinForm.java
@@ -1,35 +1,24 @@
 package com.ruoyi.rtc.pojo;
 
+import lombok.Data;
+import lombok.experimental.Accessors;
+
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 
 /**
  * @author dz
  */
+@Data
+@Accessors(chain = true)
 public class MeetingJoinForm {
     /**
-     *  会议 id
+     * 会议 id
      */
     @NotEmpty(message = "会议meetingId不能为空!")
     private String meetingId;
     /**
-     *  会议密码
+     * 会议密码
      */
     private String password;
 
-    public String getMeetingId() {
-        return meetingId;
-    }
-
-    public void setMeetingId(String meetingId) {
-        this.meetingId = meetingId;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
