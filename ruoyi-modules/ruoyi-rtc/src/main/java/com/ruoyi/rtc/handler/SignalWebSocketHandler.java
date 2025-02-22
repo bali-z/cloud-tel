c929a454acc77a2068632bc8657c178e454364d3
dmzhang <z50610233@gmail.com>
2025-02-22 14:46:24 +0800

    refactor(rtc): 删除 RTC相关的冗余代码
    - 移除了多个与 RTC 信令相关的类和接口
    - 删除了不必要的枚举和工具类- 清理了冗余的会议信息和成员相关代码

--- ruoyi-modules/ruoyi-rtc/src/main/java/com/ruoyi/rtc/handler/SignalWebSocketHandler.java
+++ ruoyi-modules/ruoyi-rtc/src/main/java/com/ruoyi/rtc/handler/SignalWebSocketHandler.java
@@ -1,16 +1,7 @@
 package com.ruoyi.rtc.handler;
 
-import com.alibaba.fastjson.JSONObject;
-import com.ruoyi.common.core.constant.Constants;
 import com.ruoyi.common.redis.generator.SnowflakeIdGenerator;
 import com.ruoyi.common.redis.service.RedisService;
-import com.ruoyi.rtc.domain.SysMeeting;
-import com.ruoyi.rtc.pojo.RtcR;
-import com.ruoyi.rtc.pojo.SignalBase;
-import com.ruoyi.rtc.pojo.SignalType;
-import com.ruoyi.rtc.pojo.forward.*;
-import com.ruoyi.rtc.pojo.handle.JoinSignal;
-import com.ruoyi.rtc.service.ISysMeetingService;
 import com.ruoyi.system.api.RemoteUserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -26,9 +17,6 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
-import static com.ruoyi.rtc.pojo.Constants.DATA;
-import static com.ruoyi.rtc.pojo.Constants.SIGNAL;
-
 /**
  * @author dz
  * describe: 基于 spring 的websocket实现
@@ -43,9 +31,6 @@ public class SignalWebSocketHandler extends TextWebSocketHandler {
      */
     @Autowired
     private SnowflakeIdGenerator snowflakeIdGenerator;
-
-    @Autowired
-    private ISysMeetingService sysMeetingService;
 
     @Autowired
     private RedisService redisService;
@@ -104,8 +89,7 @@ public class SignalWebSocketHandler extends TextWebSocketHandler {
         socketConnectionPool.put(sessionId, session);
         log.debug("连接建立成功 sessionId:{}", sessionId);
 
-        // 给当前连接发送一个 CONNECT_SUCCESS 信令，并将sessionId传递
-        sendMessage(JSONObject.toJSONString(RtcR.instance(Constants.SUCCESS, SignalType.CONNECT_SUCCESS, "", sessionId)), sessionId);
+        // todo: 给当前连接发送一个 CONNECT_SUCCESS 信令，并将sessionId传递
     }
 
     /**
@@ -126,84 +110,6 @@ public class SignalWebSocketHandler extends TextWebSocketHandler {
     public void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         String payload = message.getPayload();
         log.debug("Message from:{},message:{}", session.getAttributes().get(CURRENT_SESSION_ID_IN_WEBSOCKET_SESSION), payload);
-        try {
-            JSONObject messageObj = JSONObject.parseObject(payload);
-            String signalType = messageObj.getString(SIGNAL);
-            SignalType type = Enum.valueOf(SignalType.class, signalType);
-            JSONObject data = messageObj.getJSONObject(DATA);
-            // 接收到 type
-            switch (type) {
-                // 接收到心跳包
-                case PING -> {
-                    // 响应 pong
-                    sendMessage(JSONObject.toJSONString(RtcR.instance(Constants.SUCCESS, SignalType.PONG, "", "")), (String) session.getAttributes().get(CURRENT_SESSION_ID_IN_WEBSOCKET_SESSION));
-                    break;
-                }
-                // 接受到JOIN信令 (校验用户是否有资格参会)
-                case JOIN -> {
-                    JoinSignal.dealJoin(sysMeetingService, redisService, remoteUserService, data, session);
-                    break;
-                }
-                // 会议发起者拒绝请求加入会议的人参会
-                case JOIN_REJECT -> {
-                    JoinRejectInfo joinRejectInfo = JSONObject.toJavaObject(data, JoinRejectInfo.class);
-                    // 给目标session发送拒绝参会即可
-                    sendMessage(JSONObject.toJSONString(RtcR.instance(Constants.SUCCESS, SignalType.JOIN_REJECT, "管理员拒绝你的入会请求!", "管理员拒绝你的入会请求!")), joinRejectInfo.getTargetSessionId());
-                    break;
-                }
-                // 会议发起者同意加入会议请求转发
-                case JOIN_RESOLVE -> {
-                    JoinResolveInfo joinResolveInfo = JSONObject.toJavaObject(data, JoinResolveInfo.class);
-                    // 给请求者一个ticket,这样的话就可以顺利进入会议
-                    String meetingId = joinResolveInfo.getMeetingId();
-                    SysMeeting meetingByMeetingId = sysMeetingService.getMeetingByMeetingId(meetingId);
-                    String ticket = SignalBase.createTicket(meetingByMeetingId, joinResolveInfo.getTargetUserId());
-                    sendMessage(JSONObject.toJSONString(RtcR.instance(Constants.SUCCESS, SignalType.JOIN_RESOLVE, "agree!", ticket)), joinResolveInfo.getTargetSessionId());
-                    break;
-                }
-                // 接受到Enter信令 (参会)
-                case ENTER -> {
-                    EnterInfo.dealEnter(sysMeetingService, redisService, remoteUserService, data, session);
-                    break;
-                }
-                // offer 交换媒体流信息
-                case OFFER -> {
-                    OfferAnswer offerAnswer = JSONObject.toJavaObject(data, OfferAnswer.class);
-                    offerAnswer.setSourceSessionId((String) session.getAttributes().get(CURRENT_SESSION_ID_IN_WEBSOCKET_SESSION));
-                    // 将媒体流信息发送给对方
-                    sendMessage(JSONObject.toJSONString(RtcR.instance(Constants.SUCCESS, SignalType.OFFER, "OFFER!", offerAnswer)), offerAnswer.getTargetSessionId());
-                    break;
-                }
-                // offer 交换媒体流信息
-                case ANSWER -> {
-                    OfferAnswer offerAnswer = JSONObject.toJavaObject(data, OfferAnswer.class);
-                    offerAnswer.setSourceSessionId((String) session.getAttributes().get(CURRENT_SESSION_ID_IN_WEBSOCKET_SESSION));
-                    // 将媒体流信息发送给对方
-                    sendMessage(JSONObject.toJSONString(RtcR.instance(Constants.SUCCESS, SignalType.ANSWER, "ANSWER!", offerAnswer)), offerAnswer.getTargetSessionId());
-                    break;
-                }
-                // 交换网络信息
-                case CANDIDATE -> {
-                    CandidateInfo candidateInfo = JSONObject.toJavaObject(data, CandidateInfo.class);
-                    candidateInfo.setSourceSessionId((String) session.getAttributes().get(CURRENT_SESSION_ID_IN_WEBSOCKET_SESSION));
-                    // 将媒体流信息发送给对方
-                    sendMessage(JSONObject.toJSONString(RtcR.instance(Constants.SUCCESS, SignalType.CANDIDATE, "CANDIDATE!", candidateInfo)), candidateInfo.getTargetSessionId());
-                    break;
-                }
-                // 挂断信令
-                case HANGUP -> {
-                    HangUpInfo.dealHangUp(sysMeetingService, redisService, remoteUserService, data, session);
-                    break;
-                }
-
-                default -> {
-                    log.info("receive message sessionId:{},message:{},unknow siginal!", session.getAttributes().get(CURRENT_SESSION_ID_IN_WEBSOCKET_SESSION), payload);
-                    break;
-                }
-            }
-        } catch (Exception exception) {
-            log.error("WebSocket receive message failed,message:{} errors:{}", message, exception.getMessage());
-        }
     }
 
     @Override
@@ -219,6 +125,7 @@ public class SignalWebSocketHandler extends TextWebSocketHandler {
 
     /**
      * 使用懒汉模式，获取锁
+     * todo: 锁的清理
      *
      * @param meetingId
      * @return
