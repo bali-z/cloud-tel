052f94fcc3894f81062609a133e55c44ef5198f3
dmzhang <z50610233@gmail.com>
2025-02-23 16:37:58 +0800

    refactor(ruoyi-ui): 重构项目并适配 Vue3
    - 移除了 .editorconfig、.eslintignore 和 .eslintrc.js 文件
    - 更新了环境变量文件命名和内容- 重构了 401 和 404 组件，使用新的 script setup 语法
    - 更新了 App.vue 和 AppMain.vue 组件结构
    - 重构了 auth.js 插件，使用新的 store 访问方式
    - 更新了 authRole.vue 组件，使用新的 Vue 3 语法和逻辑

--- ruoyi-ui/src/views/system/meeting/index.vue
+++ ruoyi-ui/src/views/system/meeting/index.vue
@@ -1,90 +1,94 @@
 <template>
   <div class="app-container">
-    <el-form :model="queryParams" ref="queryForm" size="small" :inline="true" v-show="showSearch" label-width="68px">
+    <el-form :model="queryParams" ref="queryRef" :inline="true" v-show="showSearch" label-width="68px">
       <el-form-item label="会议议题" prop="title">
         <el-input
-          v-model="queryParams.title"
-          placeholder="请输入会议议题"
-          clearable
-          @keyup.enter.native="handleQuery"
+            v-model="queryParams.title"
+            placeholder="请输入会议议题"
+            clearable
+            @keyup.enter="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="会议密码" prop="password">
+        <el-input
+            v-model="queryParams.password"
+            placeholder="请输入会议密码"
+            clearable
+            @keyup.enter="handleQuery"
         />
       </el-form-item>
       <el-form-item label="会议状态" prop="status">
         <el-select v-model="queryParams.status" placeholder="请选择会议状态" clearable>
           <el-option
-            v-for="dict in dict.type.sys_meeting_status"
-            :key="dict.value"
-            :label="dict.label"
-            :value="dict.value"
+              v-for="dict in sys_meeting_status"
+              :key="dict.value"
+              :label="dict.label"
+              :value="dict.value"
           />
         </el-select>
       </el-form-item>
       <el-form-item label="计划开始" prop="planStartTime">
         <el-date-picker clearable
-          v-model="queryParams.planStartTime"
-          type="datetime"
-          value-format="yyyy-MM-dd HH:mm"
-          placeholder="请选择计划开始">
+                        v-model="queryParams.planStartTime"
+                        type="datetime"
+                        value-format="YYYY-MM-DD HH:mm"
+                        placeholder="请选择计划开始">
         </el-date-picker>
       </el-form-item>
       <el-form-item label="计划结束" prop="planEndTime">
         <el-date-picker clearable
-          v-model="queryParams.planEndTime"
-          type="datetime"
-          value-format="yyyy-MM-dd HH:mm"
-          placeholder="请选择计划结束">
+                        v-model="queryParams.planEndTime"
+                        type="datetime"
+                        value-format="YYYY-MM-DD HH:mm"
+                        placeholder="请选择计划结束">
         </el-date-picker>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">搜索</el-button>
-        <el-button icon="el-icon-refresh" size="mini" @click="resetQuery">重置</el-button>
+        <el-button type="primary" icon="Search" @click="handleQuery">搜索</el-button>
+        <el-button icon="Refresh" @click="resetQuery">重置</el-button>
       </el-form-item>
     </el-form>
 
     <el-row :gutter="10" class="mb8">
       <el-col :span="1.5">
         <el-button
-          type="primary"
-          plain
-          icon="el-icon-plus"
-          size="mini"
-          @click="handleAdd"
-          v-hasPermi="['system:meeting:add']"
+            type="primary"
+            plain
+            icon="Plus"
+            @click="handleAdd"
+            v-hasPermi="['system:meeting:add']"
         >新增</el-button>
       </el-col>
       <el-col :span="1.5">
         <el-button
-          type="success"
-          plain
-          icon="el-icon-edit"
-          size="mini"
-          :disabled="single"
-          @click="handleUpdate"
-          v-hasPermi="['system:meeting:edit']"
+            type="success"
+            plain
+            icon="Edit"
+            :disabled="single"
+            @click="handleUpdate"
+            v-hasPermi="['system:meeting:edit']"
         >修改</el-button>
       </el-col>
       <el-col :span="1.5">
         <el-button
-          type="danger"
-          plain
-          icon="el-icon-delete"
-          size="mini"
-          :disabled="multiple"
-          @click="handleDelete"
-          v-hasPermi="['system:meeting:remove']"
+            type="danger"
+            plain
+            icon="Delete"
+            :disabled="multiple"
+            @click="handleDelete"
+            v-hasPermi="['system:meeting:remove']"
         >删除</el-button>
       </el-col>
       <el-col :span="1.5">
         <el-button
-          type="warning"
-          plain
-          icon="el-icon-download"
-          size="mini"
-          @click="handleExport"
-          v-hasPermi="['system:meeting:export']"
+            type="warning"
+            plain
+            icon="Download"
+            @click="handleExport"
+            v-hasPermi="['system:meeting:export']"
         >导出</el-button>
       </el-col>
-      <right-toolbar :showSearch.sync="showSearch" @queryTable="getList"></right-toolbar>
+      <right-toolbar v-model:showSearch="showSearch" @queryTable="getList"></right-toolbar>
     </el-row>
 
     <el-table v-loading="loading" :data="meetingList" @selection-change="handleSelectionChange">
@@ -93,61 +97,49 @@
       <el-table-column label="会议议题" align="center" prop="title" />
       <el-table-column label="会议密码" align="center" prop="password" />
       <el-table-column label="会议状态" align="center" prop="status">
-        <template slot-scope="scope">
-          <dict-tag :options="dict.type.sys_meeting_status" :value="scope.row.status"/>
+        <template #default="scope">
+          <dict-tag :options="sys_meeting_status" :value="scope.row.status"/>
         </template>
       </el-table-column>
       <el-table-column label="计划开始" align="center" prop="planStartTime" width="180">
-        <template slot-scope="scope">
+        <template #default="scope">
           <span>{{ parseTime(scope.row.planStartTime, '{y}-{m}-{d} {h}:{m}') }}</span>
         </template>
       </el-table-column>
       <el-table-column label="计划结束" align="center" prop="planEndTime" width="180">
-        <template slot-scope="scope">
+        <template #default="scope">
           <span>{{ parseTime(scope.row.planEndTime, '{y}-{m}-{d} {h}:{m}') }}</span>
         </template>
       </el-table-column>
       <el-table-column label="实际开始" align="center" prop="actStartTime" width="180">
-        <template slot-scope="scope">
+        <template #default="scope">
           <span>{{ parseTime(scope.row.actStartTime, '{y}-{m}-{d} {h}:{m}') }}</span>
         </template>
       </el-table-column>
       <el-table-column label="实际结束" align="center" prop="actEndTime" width="180">
-        <template slot-scope="scope">
+        <template #default="scope">
           <span>{{ parseTime(scope.row.actEndTime, '{y}-{m}-{d} {h}:{m}') }}</span>
         </template>
       </el-table-column>
       <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
-        <template slot-scope="scope">
-          <el-button
-            size="mini"
-            type="text"
-            icon="el-icon-edit"
-            @click="handleUpdate(scope.row)"
-            v-hasPermi="['system:meeting:edit']"
-          >修改</el-button>
-          <el-button
-            size="mini"
-            type="text"
-            icon="el-icon-delete"
-            @click="handleDelete(scope.row)"
-            v-hasPermi="['system:meeting:remove']"
-          >删除</el-button>
+        <template #default="scope">
+          <el-button link type="primary" icon="Edit" @click="handleUpdate(scope.row)" v-hasPermi="['system:meeting:edit']">修改</el-button>
+          <el-button link type="primary" icon="Delete" @click="handleDelete(scope.row)" v-hasPermi="['system:meeting:remove']">删除</el-button>
         </template>
       </el-table-column>
     </el-table>
 
     <pagination
-      v-show="total>0"
-      :total="total"
-      :page.sync="queryParams.pageNum"
-      :limit.sync="queryParams.pageSize"
-      @pagination="getList"
+        v-show="total>0"
+        :total="total"
+        v-model:page="queryParams.pageNum"
+        v-model:limit="queryParams.pageSize"
+        @pagination="getList"
     />
 
     <!-- 添加或修改系统会议对话框 -->
-    <el-dialog :title="title" :visible.sync="open" width="500px" append-to-body>
-      <el-form ref="form" :model="form" :rules="rules" label-width="80px">
+    <el-dialog :title="title" v-model="open" width="500px" append-to-body>
+      <el-form ref="meetingRef" :model="form" :rules="rules" label-width="80px">
         <el-form-item label="会议议题" prop="title">
           <el-input v-model="form.title" placeholder="请输入会议议题" />
         </el-form-item>
@@ -156,189 +148,186 @@
         </el-form-item>
         <el-form-item label="计划开始" prop="planStartTime">
           <el-date-picker clearable
-            v-model="form.planStartTime"
-            type="datetime"
-            value-format="yyyy-MM-dd HH:mm"
-            placeholder="请选择计划开始">
+                          v-model="form.planStartTime"
+                          type="datetime"
+                          value-format="YYYY-MM-DD HH:mm"
+                          placeholder="请选择计划开始">
           </el-date-picker>
         </el-form-item>
         <el-form-item label="计划结束" prop="planEndTime">
           <el-date-picker clearable
-            v-model="form.planEndTime"
-            type="datetime"
-            value-format="yyyy-MM-dd HH:mm"
-            placeholder="请选择计划结束">
+                          v-model="form.planEndTime"
+                          type="datetime"
+                          value-format="YYYY-MM-DD HH:mm"
+                          placeholder="请选择计划结束">
           </el-date-picker>
         </el-form-item>
       </el-form>
-      <div slot="footer" class="dialog-footer">
-        <el-button type="primary" @click="submitForm">确 定</el-button>
-        <el-button @click="cancel">取 消</el-button>
-      </div>
+      <template #footer>
+        <div class="dialog-footer">
+          <el-button type="primary" @click="submitForm">确 定</el-button>
+          <el-button @click="cancel">取 消</el-button>
+        </div>
+      </template>
     </el-dialog>
   </div>
 </template>
 
-<script>
+<script setup name="Meeting">
 import { listMeeting, getMeeting, delMeeting, addMeeting, updateMeeting } from "@/api/system/meeting";
 
-export default {
-  name: "Meeting",
-  dicts: ['sys_meeting_status'],
-  data() {
-    return {
-      // 遮罩层
-      loading: true,
-      // 选中数组
-      ids: [],
-      // 非单个禁用
-      single: true,
-      // 非多个禁用
-      multiple: true,
-      // 显示搜索条件
-      showSearch: true,
-      // 总条数
-      total: 0,
-      // 系统会议表格数据
-      meetingList: [],
-      // 弹出层标题
-      title: "",
-      // 是否显示弹出层
-      open: false,
-      // 查询参数
-      queryParams: {
-        pageNum: 1,
-        pageSize: 10,
-        title: null,
-        holdUserId: null,
-        status: null,
-        planStartTime: null,
-        planEndTime: null,
-        actStartTime: null,
-        actEndTime: null
-      },
-      // 表单参数
-      form: {},
-      // 表单校验
-      rules: {
-        title: [
-          { required: true, message: "会议议题不能为空", trigger: "blur" }
-        ],
-        holdUserId: [
-          { required: true, message: "会议主不能为空", trigger: "blur" }
-        ],
-        status: [
-          { required: true, message: "会议状态不能为空", trigger: "change" }
-        ],
-        planStartTime: [
-          { required: true, message: "计划开始不能为空", trigger: "blur" }
-        ],
-        planEndTime: [
-          { required: true, message: "计划结束不能为空", trigger: "blur" }
-        ],
-      }
-    };
+const { proxy } = getCurrentInstance();
+const { sys_meeting_status } = proxy.useDict('sys_meeting_status');
+
+const meetingList = ref([]);
+const open = ref(false);
+const loading = ref(true);
+const showSearch = ref(true);
+const ids = ref([]);
+const single = ref(true);
+const multiple = ref(true);
+const total = ref(0);
+const title = ref("");
+
+const data = reactive({
+  form: {},
+  queryParams: {
+    pageNum: 1,
+    pageSize: 10,
+    title: null,
+    password: null,
+    status: null,
+    planStartTime: null,
+    planEndTime: null,
   },
-  created() {
-    this.getList();
-  },
-  methods: {
-    /** 查询系统会议列表 */
-    getList() {
-      this.loading = true;
-      listMeeting(this.queryParams).then(response => {
-        this.meetingList = response.rows;
-        this.total = response.total;
-        this.loading = false;
-      });
-    },
-    // 取消按钮
-    cancel() {
-      this.open = false;
-      this.reset();
-    },
-    // 表单重置
-    reset() {
-      this.form = {
-        meetingId: null,
-        title: null,
-        holdUserId: null,
-        status: null,
-        planStartTime: null,
-        planEndTime: null,
-        actStartTime: null,
-        actEndTime: null
-      };
-      this.resetForm("form");
-    },
-    /** 搜索按钮操作 */
-    handleQuery() {
-      this.queryParams.pageNum = 1;
-      this.getList();
-    },
-    /** 重置按钮操作 */
-    resetQuery() {
-      this.resetForm("queryForm");
-      this.handleQuery();
-    },
-    // 多选框选中数据
-    handleSelectionChange(selection) {
-      this.ids = selection.map(item => item.meetingId)
-      this.single = selection.length!==1
-      this.multiple = !selection.length
-    },
-    /** 新增按钮操作 */
-    handleAdd() {
-      this.reset();
-      this.open = true;
-      this.title = "添加系统会议";
-    },
-    /** 修改按钮操作 */
-    handleUpdate(row) {
-      this.reset();
-      const meetingId = row.meetingId || this.ids
-      getMeeting(meetingId).then(response => {
-        this.form = response.data;
-        this.open = true;
-        this.title = "修改系统会议";
-      });
-    },
-    /** 提交按钮 */
-    submitForm() {
-      this.$refs["form"].validate(valid => {
-        if (valid) {
-          if (this.form.meetingId != null) {
-            updateMeeting(this.form).then(response => {
-              this.$modal.msgSuccess("修改成功");
-              this.open = false;
-              this.getList();
-            });
-          } else {
-            addMeeting(this.form).then(response => {
-              this.$modal.msgSuccess("新增成功");
-              this.open = false;
-              this.getList();
-            });
-          }
-        }
-      });
-    },
-    /** 删除按钮操作 */
-    handleDelete(row) {
-      const meetingIds = row.meetingId || this.ids;
-      this.$modal.confirm('是否确认删除系统会议编号为"' + meetingIds + '"的数据项？').then(function() {
-        return delMeeting(meetingIds);
-      }).then(() => {
-        this.getList();
-        this.$modal.msgSuccess("删除成功");
-      }).catch(() => {});
-    },
-    /** 导出按钮操作 */
-    handleExport() {
-      this.download('system/meeting/export', {
-        ...this.queryParams
-      }, `meeting_${new Date().getTime()}.xlsx`)
-    }
+  rules: {
+    title: [
+      { required: true, message: "会议议题不能为空", trigger: "blur" }
+    ],
+    holdUserId: [
+      { required: true, message: "会议发起人不能为空", trigger: "blur" }
+    ],
+    status: [
+      { required: true, message: "会议状态不能为空", trigger: "change" }
+    ],
+    planStartTime: [
+      { required: true, message: "计划开始不能为空", trigger: "blur" }
+    ],
+    planEndTime: [
+      { required: true, message: "计划结束不能为空", trigger: "blur" }
+    ],
   }
-};
+});
+
+const { queryParams, form, rules } = toRefs(data);
+
+/** 查询系统会议列表 */
+function getList() {
+  loading.value = true;
+  listMeeting(queryParams.value).then(response => {
+    meetingList.value = response.rows;
+    total.value = response.total;
+    loading.value = false;
+  });
+}
+
+// 取消按钮
+function cancel() {
+  open.value = false;
+  reset();
+}
+
+// 表单重置
+function reset() {
+  form.value = {
+    meetingId: null,
+    title: null,
+    password: null,
+    holdUserId: null,
+    status: null,
+    planStartTime: null,
+    planEndTime: null,
+    actStartTime: null,
+    actEndTime: null
+  };
+  proxy.resetForm("meetingRef");
+}
+
+/** 搜索按钮操作 */
+function handleQuery() {
+  queryParams.value.pageNum = 1;
+  getList();
+}
+
+/** 重置按钮操作 */
+function resetQuery() {
+  proxy.resetForm("queryRef");
+  handleQuery();
+}
+
+// 多选框选中数据
+function handleSelectionChange(selection) {
+  ids.value = selection.map(item => item.meetingId);
+  single.value = selection.length != 1;
+  multiple.value = !selection.length;
+}
+
+/** 新增按钮操作 */
+function handleAdd() {
+  reset();
+  open.value = true;
+  title.value = "添加系统会议";
+}
+
+/** 修改按钮操作 */
+function handleUpdate(row) {
+  reset();
+  const _meetingId = row.meetingId || ids.value
+  getMeeting(_meetingId).then(response => {
+    form.value = response.data;
+    open.value = true;
+    title.value = "修改系统会议";
+  });
+}
+
+/** 提交按钮 */
+function submitForm() {
+  proxy.$refs["meetingRef"].validate(valid => {
+    if (valid) {
+      if (form.value.meetingId != null) {
+        updateMeeting(form.value).then(response => {
+          proxy.$modal.msgSuccess("修改成功");
+          open.value = false;
+          getList();
+        });
+      } else {
+        addMeeting(form.value).then(response => {
+          proxy.$modal.msgSuccess("新增成功");
+          open.value = false;
+          getList();
+        });
+      }
+    }
+  });
+}
+
+/** 删除按钮操作 */
+function handleDelete(row) {
+  const _meetingIds = row.meetingId || ids.value;
+  proxy.$modal.confirm('是否确认删除系统会议编号为"' + _meetingIds + '"的数据项？').then(function() {
+    return delMeeting(_meetingIds);
+  }).then(() => {
+    getList();
+    proxy.$modal.msgSuccess("删除成功");
+  }).catch(() => {});
+}
+
+/** 导出按钮操作 */
+function handleExport() {
+  proxy.download('system/meeting/export', {
+    ...queryParams.value
+  }, `meeting_${new Date().getTime()}.xlsx`)
+}
+
+getList();
 </script>
