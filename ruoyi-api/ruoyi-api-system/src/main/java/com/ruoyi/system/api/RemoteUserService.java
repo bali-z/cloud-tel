03b962b061f2b6f8d78a70b654a513a809cbc0f0
dmzhang <z50610233@gmail.com>
2025-03-14 22:02:25 +0800

    Revert "feat(system): 添加用户、角色和部门相关接口及降级处理- 在 RemoteUserService 接口中添加了以下方法："
    This reverts commit cf32005a278d1d2c338f435b4de0dfed2b3f11dc.

--- ruoyi-api/ruoyi-api-system/src/main/java/com/ruoyi/system/api/RemoteUserService.java
+++ ruoyi-api/ruoyi-api-system/src/main/java/com/ruoyi/system/api/RemoteUserService.java
@@ -1,7 +1,5 @@
 package com.ruoyi.system.api;
 
-import com.ruoyi.system.api.domain.SysDept;
-import com.ruoyi.system.api.domain.SysRole;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,8 +13,6 @@ import com.ruoyi.system.api.domain.SysUser;
 import com.ruoyi.system.api.factory.RemoteUserFallbackFactory;
 import com.ruoyi.system.api.model.LoginUser;
 
-import java.util.List;
-
 /**
  * 用户服务
  * 
@@ -25,44 +21,6 @@ import java.util.List;
 @FeignClient(contextId = "remoteUserService", value = ServiceNameConstants.SYSTEM_SERVICE, fallbackFactory = RemoteUserFallbackFactory.class)
 public interface RemoteUserService
 {
-    /**
-     * 通过角色id获取用户信息列表
-     */
-    @GetMapping("/user/listByRoleIds/{roleIds}")
-    public R<List<SysUser>> listByRoleIds(@PathVariable("roleIds") List<Long> roleIds,@RequestHeader(SecurityConstants.FROM_SOURCE) String source);
-
-    /**
-     *  通过部门id获取用户信息列表
-     */
-    @GetMapping("/user/listByDeptIds/{deptIds}")
-    public R<List<SysUser>> listByDeptIds(@PathVariable("deptIds") List<Long> deptIds,@RequestHeader(SecurityConstants.FROM_SOURCE) String source);
-
-
-    /**
-     *  根据部门id获取部门详细信息
-     */
-    @GetMapping("/dept/getDeptByDeptId/{deptId}")
-    public R<SysDept> getDeptByDeptId(@PathVariable("deptId") Long deptId,@RequestHeader(SecurityConstants.FROM_SOURCE) String source);
-
-    /**
-     *  根据角色id获取角色详细信息
-     * @param roleId
-     * @param source
-     * @return
-     */
-    @GetMapping("/role/getRoleByRoleId/{roleId}")
-    public R<SysRole> getRoleByRoleId(@PathVariable("roleId") Long roleId, @RequestHeader(SecurityConstants.FROM_SOURCE) String source);
-
-    /**
-     *  根据用户id获取用户详细信息
-     * @param userId
-     * @param source
-     * @return
-     */
-    @GetMapping("/user/infoByUserId/{userId}")
-    public R<SysUser> getUserInfoByUserId(@PathVariable("userId") Long userId, @RequestHeader(SecurityConstants.FROM_SOURCE) String source);
-
-
     /**
      * 通过用户名查询用户信息
      *
--- ruoyi-api/ruoyi-api-system/src/main/java/com/ruoyi/system/api/factory/RemoteUserFallbackFactory.java
+++ ruoyi-api/ruoyi-api-system/src/main/java/com/ruoyi/system/api/factory/RemoteUserFallbackFactory.java
@@ -1,7 +1,5 @@
 package com.ruoyi.system.api.factory;
 
-import com.ruoyi.system.api.domain.SysDept;
-import com.ruoyi.system.api.domain.SysRole;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.openfeign.FallbackFactory;
@@ -10,8 +8,6 @@ import com.ruoyi.common.core.domain.R;
 import com.ruoyi.system.api.RemoteUserService;
 import com.ruoyi.system.api.domain.SysUser;
 import com.ruoyi.system.api.model.LoginUser;
-
-import java.util.List;
 
 /**
  * 用户服务降级处理
@@ -29,31 +25,6 @@ public class RemoteUserFallbackFactory implements FallbackFactory<RemoteUserServ
         log.error("用户服务调用失败:{}", throwable.getMessage());
         return new RemoteUserService()
         {
-            @Override
-            public R<List<SysUser>> listByRoleIds(List<Long> roleIds, String source) {
-                return R.fail("获取用户失败:" + throwable.getMessage());
-            }
-
-            @Override
-            public R<List<SysUser>> listByDeptIds(List<Long> deptIds, String source) {
-                return R.fail("获取用户失败:" + throwable.getMessage());
-            }
-
-            @Override
-            public R<SysDept> getDeptByDeptId(Long deptId, String source) {
-                return R.fail("获取部门失败:" + throwable.getMessage());
-            }
-
-            @Override
-            public R<SysRole> getRoleByRoleId(Long roleId, String source) {
-                return R.fail("获取角色失败:" + throwable.getMessage());
-            }
-
-            @Override
-            public R<SysUser> getUserInfoByUserId(Long userId, String source) {
-                return R.fail("获取用户失败:" + throwable.getMessage());
-            }
-
             @Override
             public R<LoginUser> getUserInfo(String username, String source)
             {
