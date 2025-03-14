99e38f7b59a537b81eda2f79b9d0ef3ee16fe2b4
dmzhang <z50610233@gmail.com>
2025-03-14 22:02:26 +0800

    Revert "feat(workflow): 新增工作流模块基础功能"
    This reverts commit 9c14596fb389098efd45a43c1108df3740f5448c.

--- ruoyi-modules/ruoyi-workflow/src/main/java/com/ruoyi/workflow/common/validate/AddGroup.java
+++
@@ -1,9 +0,0 @@
-package com.ruoyi.workflow.common.validate;
-
-/**
- * 校验分组 add
- *
- * @author Lion Li
- */
-public interface AddGroup {
-}
--- ruoyi-modules/ruoyi-workflow/src/main/java/com/ruoyi/workflow/common/validate/QueryGroup.java
+++
@@ -1,9 +0,0 @@
-package com.ruoyi.workflow.common.validate;
-
-/**
- * 校验分组 query
- *
- * @author Lion Li
- */
-public interface QueryGroup {
-}
